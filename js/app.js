96189ed6be366d8dcd08b91ff03877597885412d
Priyojit Kharibam <priyojitkharibam@gmail.com>
2019-06-19 17:29:05 +0530

    stored in set

--- js/app.js
+++ js/app.js
@@ -1,20 +1,43 @@
-$(document).ready(function () {
-    $("#myInput").focus();
-    $("button").click(function () {
-        var res = $("#myInput").val();
-        $("#result").text(res);
-        $("#myInput").val("");
-        $("#history").append("<span class=\"w3-padding-small\"> " + res + " </span>");
-    });
+$(document).ready(function() {
+  let historyValue = new Set();
+  $("#myInput").focus();
 
-    $('#myInput').keypress(function (e) {
-        var key = e.which;
-        if (key == 13) // the enter key code
-        {
-            var res = $("#myInput").val();
-            $("#result").text(res).fadeIn();
-            $("#myInput").val("");
-            $("#history").append("<span class=\" w3-padding-small \"> " + res +" </span>");
-        }
-    });
+  $("button").click(function() {
+    var res = $("#myInput").val();
+
+    if (res !== "" && !historyValue.has(res)) {
+      historyValue.add(res);
+      $("#result").removeClass("w3-text-red");
+      $("#result").text(res);
+      $("#myInput").val("");
+      $("#history").append(
+        '<span class="w3-padding-small"> ' + res + " </span>"
+      );
+    } else {
+      $("#result").addClass("w3-text-red");
+    }
+    $("#myInput").focus();
+  });
+
+  $("#myInput").keypress(function(e) {
+    var key = e.which;
+    if (key == 13) {
+      // the enter key code
+      var res = $("#myInput").val();
+      if (res !== "" && !historyValue.has(res)) {
+        historyValue.add(res);
+        $("#result").removeClass("w3-text-red");
+        $("#result")
+          .text(res)
+          .fadeIn();
+        $("#myInput").val("");
+        $("#history").append(
+          '<span class="w3-padding-small "> ' + res + " </span>"
+        );
+      } else {
+        $("#result").addClass("w3-text-red");
+      }
+    }
+    $("#myInput").focus();
+  });
 });
